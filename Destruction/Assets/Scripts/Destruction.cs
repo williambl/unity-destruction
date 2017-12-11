3167071db4f61fa40b96520a85b344266f0f0668
williambl <william@williambl.com>
2017-12-11 21:20:47 +0000

    Added a script to break or put back together objects

---
+++ Destruction/Assets/Scripts/Destruction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destruction : MonoBehaviour {
+
+    public GameObject brokenObj;
+    public GameObject togetherObj;
+
+    // True if together, false if broken
+    public bool together = true;
+
+    // Use this for initialization
+    void Start () {
+        togetherObj.SetActive(true);
+        brokenObj.SetActive(false);        
+    }
+	
+    // Update is called once per frame
+    void Update () {
+        togetherObj.SetActive(together);
+        brokenObj.SetActive(!together);
+    }
+
+}
