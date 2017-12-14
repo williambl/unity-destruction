bd9fb08ac2ba93fdace3fcf8ddbf6faa48e344ee
williambl <william@williambl.com>
2017-12-14 15:33:31 +0000

    Added a script to throw a ball at an object

---
+++ Destruction/Assets/example-game/Scripts/ThrowBall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowBall : MonoBehaviour {
+
+    public GameObject ball;
+    public float forceMultiplier;
+    Camera camera;
+
+    void Start () {
+        camera = GetComponent<Camera>();
+    }
+	
+    void Update () {
+        if (Input.GetButtonDown("Fire1")) {
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            GameObject newBall = Instantiate(ball, ray.origin, Quaternion.Euler(Vector3.zero));
+            newBall.GetComponent<Rigidbody>().AddForce(ray.direction * forceMultiplier);
+        }
+    }
+}
